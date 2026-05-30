7902529e76062b82341fd55c3d204307c00b11e4
Jose-11-2001 <josephmbukwa2001@gmail.com>
2026-05-30 20:47:44 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,8 +13,6 @@ function Home() {
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-60"></div>
-      
-      {/* Top Navigation Bar */}
       <div className="relative z-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex justify-end">
