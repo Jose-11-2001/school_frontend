d225807a647c8298a8f994dbef7cdc58690d387b
Jose-11-2001 <josephmbukwa2001@gmail.com>
2026-07-22 22:53:20 +0200

    adding deputy file

--- src/components/Rankings.jsx
+++ src/components/Rankings.jsx
@@ -261,7 +261,7 @@ function Rankings() {
         </div>
       )}
 
-      {/* Loading State */}
+      
       {loading && (
         <div className="bg-white rounded-lg shadow p-8 text-center">
           <div className="inline-block animate-spin rounded-full h-8 w-8 border-b-2 border-blue-500 mb-4"></div>
