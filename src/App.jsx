22edf8230f6202ac3fbfe649c9e327e102f2f99d
Jose-11-2001 <josephmbukwa2001@gmail.com>
2026-08-02 00:07:21 +0200

    upa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/common/Layout';
-import Home from './components/common/Home';
+import Layout from './components/Common/Layout';
+import Home from './components/Common/Home';
 import Login from './components/Auth/Login';
 import ChangePassword from './components/Auth/ChangePassword';
 import AdminDashboard from './components/Admin/AdminDashboard';
